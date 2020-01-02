6cd83d34b04ae5d3e138968e676adb79b545223d
Samruddhi Purandare <spurandare@expedia.com>
2020-01-02 11:30:24 -0800

    To destroy dashboards

---
+++ grafana-dashboard-sam.tf
@@ -0,0 +1,4 @@
+provider "grafana" {
+  url  = "https://grafana.egdl-eks-us-east-1.egdp-dev.aws.away.black"
+  auth = "eyJrIjoiMkcyY3VjSlMwaUZRSFpZVTQ0SXo1ZDZRRkVYckhIUzAiLCJuIjoic2FtcnVkZGhpIiwiaWQiOjF9"
+}

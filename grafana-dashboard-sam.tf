5ce410291a6dc6d4d0073568b93aaca47aefa5a3
Samruddhi Purandare <spurandare@expedia.com>
2020-01-02 10:57:44 -0800

    Test

--- grafana-dashboard-sam.tf
+++ grafana-dashboard-sam.tf
@@ -5,7 +5,19 @@ provider "grafana" {
 resource "grafana_dashboard" "sam-test-tf" {
   config_json = "${file("${path.module}/templates/grafana-sam-test.json")}"
 }
+resource "kubernetes_config_map" "grafana-dashboard" {
+ metadata {
+  name = "sam-tf-dashboard"
+  namespace = "monitoring"
+  labels = {
+   grafana_dashboard="true"
+  }
+ }
 
+ data = {
+  "dashboard-test.json" = file("${path.module}/templates/grafana-sam-test.json")
+ }
+}
 # resource "grafana_data_source" "test_cloudwatch-sam-tf" {
 #   type = "cloudwatch"
 #   name = "cw-example"

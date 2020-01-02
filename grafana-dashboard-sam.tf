405e0f383921261097e72bb1eeb7debffec7aeab
Samruddhi Purandare <spurandare@expedia.com>
2020-01-02 11:12:23 -0800

    trying with wrong api key

--- grafana-dashboard-sam.tf
+++ grafana-dashboard-sam.tf
@@ -1,23 +1,11 @@
 provider "grafana" {
   url  = "https://grafana.egdl-eks-us-east-1.egdp-dev.aws.away.black"
-  auth = "eyJrIjoiMkcyY3VjSlMwaUZRSFpZVTQ0SXo1ZDZRRkVYckhIUzAiLCJuIjoic2FtcnVkZGhpIiwiaWQiOjF9"
+  auth = "123"
 }
 resource "grafana_dashboard" "sam-test-tf" {
   config_json = "${file("${path.module}/templates/grafana-sam-test.json")}"
 }
-resource "kubernetes_config_map" "grafana-dashboard" {
- metadata {
-  name = "sam-tf-dashboard"
-  namespace = "monitoring"
-  labels = {
-   grafana_dashboard="true"
-  }
- }
 
- data = {
-  "dashboard-test.json" = file("${path.module}/templates/grafana-sam-test.json")
- }
-}
 # resource "grafana_data_source" "test_cloudwatch-sam-tf" {
 #   type = "cloudwatch"
 #   name = "cw-example"
